ca35158fe3d32da4ea781e2081f81aa7d7f27356
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-02-17 18:17:26 -0300

    Add base structure to recursive digit sum

---
+++ src/RecursiveDigitSum.java
@@ -0,0 +1,12 @@
+public class RecursiveDigitSum {
+
+  public static void main(String[] args) {
+    RecursiveDigitSum r = new RecursiveDigitSum();
+    int sum = r.sum("150", 3);
+  }
+
+  private int sum(String s, int k) {
+
+    return 1;
+  }
+}
