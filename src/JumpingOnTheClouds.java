d0a08705d526599c2a3523adb03a6fb3cfc695d7
tomas.lingotti <tomas.lingotti@globant.com>
2019-02-18 19:20:45 -0300

    Add basic structure to Jumping in the clouds

---
+++ src/JumpingOnTheClouds.java
@@ -0,0 +1,13 @@
+public class JumpingOnTheClouds {
+
+  public static void main(String[] args) {
+    JumpingOnTheClouds j = new JumpingOnTheClouds();
+    int[] arr = new int[] {0,0,1,0,0,1,1,0};
+    int energy = j.jumps(arr, 2);
+    System.out.println(energy);
+  }
+
+  private int jumps(int[] clouds, int k) {
+    return 1;
+  }
+}
