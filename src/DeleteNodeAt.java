79796f68d20cecb36a09492bfdaaea7313b10bed
Tomas Lingotti <tomaslingotti@gmail.com>
2019-02-23 21:17:58 -0300

    Delete node at a position

---
+++ src/DeleteNodeAt.java
@@ -0,0 +1,18 @@
+public class DeleteNodeAt {
+
+    public static void main(final String[] args) {
+        final DeleteNodeAt deleteNodeAt = new DeleteNodeAt();
+    }
+
+    /**
+     * <a href="https://www.hackerrank.com/challenges/delete-a-node-from-a-linked-list/problem">link</a>
+     *
+     * @param llist    The linked list.
+     * @param position The position for the node to delete
+     * @return The linked list with the node deleted.
+     */
+    private SinglyLinkedListNode deleteNode(final SinglyLinkedListNode llist, final int position) {
+
+        return llist;
+    }
+}
