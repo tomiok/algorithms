9c06ff00fda755e7cbd051c6f2fb0199e17144e3
tomiok <tomaslingotti@gmail.com>
2019-03-04 14:57:22 -0300

    Sock merchant

---
+++ src/SockMerchant.java
@@ -0,0 +1,27 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class SockMerchant {
+
+  public static void main(String[] args) {
+    SockMerchant s = new SockMerchant();
+    int[] arr = {10, 20, 20, 10, 10, 30, 50, 10, 20};
+    int res = s.sock(9, arr);
+    System.out.println(res);
+  }
+
+  private int sock(int n, int[] colours) {
+    Set<Integer> s = new HashSet<>(n);
+    int c = 0;
+    for (int i: colours) {
+      if (!s.add(i)) {
+        s.remove(i);
+        c++;
+      } else {
+        s.add(i);
+      }
+
+    }
+    return c;
+  }
+}
