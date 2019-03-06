8434570cc0d4910cde516d80a52ab9993bc3d8ff
tomiok <tomaslingotti@gmail.com>
2019-03-06 14:19:21 -0300

    Jumping clouds

---
+++ src/JumpingClouds.java
@@ -0,0 +1,32 @@
+public class JumpingClouds {
+
+  public static void main(String[] args) {
+    JumpingClouds clouds = new JumpingClouds();
+    int[] arr = {0, 0, 1, 0, 0, 0, 0, 1, 0};
+    int res = clouds.jump(arr);
+    System.out.println(res);
+  }
+
+  private int jump(int[] arr) {
+    int jumps = 0;
+    boolean chain = false;
+    for (int i = 1; i < arr.length; i++) {
+      if (arr[i] == 0) {
+
+        jumps++;
+        if (i == 1) {
+          continue;
+        }
+
+        if (arr[i-1] == 0 && arr[i-2] == 0 & !chain) {
+          jumps--;
+          chain = true;
+        } else {
+          chain = false;
+        }
+      }
+
+    }
+    return jumps;
+  }
+}
