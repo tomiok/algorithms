97f56343e45b4777466ede3ec720031f28ddd1a3
tomiok <tomaslingotti@gmail.com>
2019-03-06 13:26:36 -0300

    Add valley problem

---
+++ src/CountingValley.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class CountingValley {
+
+  public static void main(String[] args) {
+    CountingValley cv = new CountingValley();
+  }
+
+  /**
+   * <a href="https://www.hackerrank.com/challenges/counting-valleys/problem">link</a>
+   *
+   * @param steps Number of steps
+   * @param moves [D,U] Down, Up
+   * @return Num of valleys, a valley is a below-sea-level place. Starting at sea level.
+   */
+  private int count(int steps, String moves) {
+    int v = 0;     // # of valleys
+    int lvl = 0;   // current level
+    for(char c : moves.toCharArray()){
+      if(c == 'U') ++lvl;
+      if(c == 'D') --lvl;
+
+      // if we just came UP to sea level
+      if(lvl == 0 && c == 'U')
+        ++v;
+    }
+    return v;
+  }
+}
