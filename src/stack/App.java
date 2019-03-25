93115c31505987f5be11a15860370670e398d3b4
Tomas Lingotti <tomaslingotti@hotmail.com>
2019-03-25 01:03:46 -0300

    implementing stack - II

---
+++ src/stack/App.java
@@ -0,0 +1,8 @@
+package stack;
+
+public class App {
+
+  public static void main(String[] args) {
+    Stack<Integer> stack = new Stack<>();
+  }
+}
--- src/stack/Stack.java
+++ src/stack/Stack.java
@@ -6,9 +6,30 @@ public class Stack<T extends Comparable<T>> {
 
   private int count;
 
+  private void push(T data) {
+    count++;
+
+    if (isEmpty()) {
+      this.node = new Node<>(data);
+    } else {
+      Node<T> n = new Node<>(data);
+      n.setNext(node);
+      node = n;
+    }
+  }
+
+  private T pop() {
+    count--;
+    Node<T> toPop = node.getNext();
+    node = toPop;
+    return toPop.getData();
+  }
+
   private boolean isEmpty() {
     return this.node == null;
   }
 
-  
+  private int size() {
+    return this.count;
+  }
 }
